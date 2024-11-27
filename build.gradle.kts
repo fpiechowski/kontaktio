09ade696fe24cfc58ed197e242e7fbbe78a3ee57
Filip Piechowski <f.piechowski@gmail.com>
2024-11-27 17:16:51 +0100

    Kotlin Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+﻿plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "com.github.fpiechowski"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
