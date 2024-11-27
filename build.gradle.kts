c4c50e9754c73c2fb5bd1164f98acfbba76e9b1d
Filip Piechowski <f.piechowski@gmail.com>
2024-11-27 17:29:21 +0100

    shadowJar and application plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 ﻿plugins {
     kotlin("jvm") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "com.github.fpiechowski"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.github.fpiechowski.kontaktio.MainKt")
+}
 
 repositories {
     mavenCentral()
